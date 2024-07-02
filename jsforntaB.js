4ea2c7a3d7b27829bf837e49f8e3f0cb892b84a1
spade1345 <rbmbe4@gmail.com>
2024-07-02 17:34:58 -0500

    Added my own stuff

---
+++ jsforntaB.js
@@ -0,0 +1,9 @@
+now = new Date();
+time = now.toString();
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1);
